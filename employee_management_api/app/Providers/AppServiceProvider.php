b55b2b023a97274c23bf5a3c8ee78452f3867b12
MectronPrime <you@example.com>
2026-03-19 15:34:38 +0530

    Revert "bind employee implementation and interface  in app service provider |"
    This reverts commit 34683bdef7ad0d5d4a29e0eb65d0cb611172d329.

--- employee_management_api/app/Providers/AppServiceProvider.php
+++ employee_management_api/app/Providers/AppServiceProvider.php
@@ -8,8 +8,6 @@ use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
-use Modules\Employee\app\Repositories\Employeeimplementation;
-use Modules\Employee\Repositories\EmployeeInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,11 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //Bind interface to concrete repository implementation
-         $this->app->bind(
-            EmployeeInterface::class,
-            EmployeeImplementation::class
-        );
+        //
     }
 
     /**
@@ -30,9 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191);
-        $this->loadMigrationsFrom(module_path('Employees', 'Database/Migrations'));
-
+       Schema::defaultStringLength(191);
     }
 
     /**
